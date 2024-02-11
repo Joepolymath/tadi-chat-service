5520756e3ca7930165c3c2af40c0dd3d9922fa40
Joepolymath <joshuaajagbe96@gmail.com>
2024-02-11 01:10:36 +0100

    basic server setup

---
+++ Makefile
@@ -0,0 +1,38 @@
+# Makefile for Go project
+
+# Variables
+APP_NAME := app
+SRC_DIR := .
+BUILD_DIR := .
+BIN_DIR := bin
+PKG_DIR := pkg
+MAIN_FILE := main.go
+
+# Commands
+GO := go
+GO_BUILD := $(GO) build
+GO_RUN := $(GO) run
+GO_CLEAN := $(GO) clean
+GO_TEST := $(GO) test
+
+# Targets
+.PHONY: all build run clean test
+
+all: build
+
+build:
+	@echo "Building $(APP_NAME)..."
+	$(GO_BUILD) -o $(BUILD_DIR)/$(APP_NAME) $(SRC_DIR)/$(MAIN_FILE)
+
+run:
+	@echo "Running $(APP_NAME)..."
+	$(GO_RUN) $(SRC_DIR)/$(MAIN_FILE)
+
+clean:
+	@echo "Cleaning up..."
+	$(GO_CLEAN)
+	rm -rf $(BUILD_DIR) $(BIN_DIR) $(PKG_DIR)
+
+test:
+	@echo "Running tests..."
+	$(GO_TEST) ./...
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	WriteTimeout = 90
+	ReadTimeout = 90
+)
+
+// Global vars for versioning
+var (
+	Build     = "unset" // nolint
+	BuildDate = "unset" // nolint
+	GoVersion = "unset" // nolint
+	Version   = "unset" // nolint
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "OK %s %s %s %s", Build, BuildDate, GoVersion, Version)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	srv := http.Server{
+		Handler: r,
+		Addr: "0.0.0.0:3004",
+		WriteTimeout: WriteTimeout * time.Second,
+		ReadTimeout: ReadTimeout * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
